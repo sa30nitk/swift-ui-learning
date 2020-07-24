b15906f70faa6538527faacc1a5e50b5311258a2
sateesh.g <sateesh.g@go-jek.com>
2020-07-24 22:23:20 +0530

    Initial Commit

---
+++ Landmark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark
+//
+//  Created by sateesh Gudla on 24/07/20.
+//  Copyright © 2020 Sateesh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
